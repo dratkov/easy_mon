7102761324f2dab734e0babc1a73b92c136b3021
dratkov <ratkovdima@mail.ru>
2012-09-24 20:40:28 +0400

    text block

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-from monitorings.models import Menu, Article
+from monitorings.models import Menu, Article, TextBlock
 
 
 def my_global_vars(request):
     menu_res = Menu.objects.order_by('-order')
     last_article = Article.objects.order_by('-date_published')[:3]
+    text_blocks = TextBlock.objects.filter(is_main=0)
     return {
-            'site_title': u"Мониторинг", 'menu_res': menu_res, 'last_article': last_article
+            'site_title': u"Мониторинг", 'menu_res': menu_res, 'last_article': last_article, 'text_blocks': text_blocks
     }
