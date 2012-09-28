f06344a71ac6df051ed1ed9527e64470c959bcb5
dratkov <ratkovdima@mail.ru>
2012-09-28 12:35:46 +0400

    my_tags

--- monitorings/templatetags/my_tags.py
+++ monitorings/templatetags/my_tags.py
@@ -7,6 +7,6 @@ register = template.Library()
 def main_menu_active(request, page_id):
     print request.path
     print page_id
-    if len(request.path.split("/")) > 2 and int(page_id) == int(request.path.split("/")[2]):
+    if len(request.path.split("/")) > 2 and page_id == request.path.split("/")[2]:
         return 'active current'
     return ''
